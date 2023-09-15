88c83635475ecb6df6dd0787c775a7eec7c01866
Luke Rieff <luke.rieff@gmail.com>
2023-09-15 12:19:41 +0200

    use C++ 23 becaus C++ 17 is gay

--- Scheduler.h
+++ Scheduler.h
@@ -5,10 +5,17 @@
 #ifndef BEROEPSPRODUCT_SCHEDULER_H
 #define BEROEPSPRODUCT_SCHEDULER_H
 
+#include <map>
+#include <list>
+
+#include "Task.h"
+#include "Job.h"
+#include "Machine.h"
 
 class Scheduler {
 private:
-
+    std::map<unsigned int, Machine> mMachines;
+    std::list<Job> mJobs;
 };
 
 
